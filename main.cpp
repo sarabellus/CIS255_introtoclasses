733c7eec824cb8abcb2ae2b272be26de9f8fec59
sarabellus <108199826+sarabellus@users.noreply.github.com>
2022-07-04 19:47:57 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 #include <string>
 #include "student.h"
+#include "gradStudent.h"
 using namespace std;
-
 
 void displayData(student mystudent)
 {
@@ -13,13 +13,48 @@ void displayData(student mystudent)
     cout << "GPA " << mystudent.GPA() << endl;
     cout << "Credits Earned: " << mystudent.creditsTaken() << endl;
     cout << "Class Standing: " << mystudent.standing() << endl;
+    cout << endl;
 }
-
+void displayData(GradStudent mygradstudent)
+{
+    cout << setprecision(2) << fixed;
+    cout << mygradstudent.fName() << "  " << mygradstudent.lName() << endl;
+    cout << "GPA " << mygradstudent.GPA() << endl;
+    cout << "Credits Earned: " << mygradstudent.creditsTaken() << endl;
+    cout << "Class Standing: " << mygradstudent.standing() << endl;
+    cout << "TA Pay: $" << mygradstudent.TApay() << endl;
+}
 int main()
 {
     string fName, lName, standing;
-    float gpa, credits;
+    float gpa, credits, tapay;
     student mystudent;
+    GradStudent mygradstudent;
+    
+    cout << "Undergraduate Student:" << endl;
+    displayData(mystudent);
+    cout << endl << endl;
+    
+    cout << "Undergraduate:" << endl;
+    cout << "Enter first name: ";
+    cin >> fName;
+    cout << endl << "Enter last name: ";
+    cin >> lName;
+    cout << endl << "Enter GPA: ";
+    cin >> gpa;
+    cout << endl << "Enter credits taken: ";
+    cin >> credits;
+    cout << endl;
+    
+    mystudent.fName(fName);
+    mystudent.lName(lName);
+    mystudent.GPA(gpa);
+    mystudent.creditsTaken(credits);
+    mystudent.standing(credits);
+    displayData(mystudent);
+    
+    cout << "Graduate Student:";
+    cout << endl << endl;
     
     cout << "Enter first name: ";
     cin >> fName;
@@ -29,13 +64,16 @@ int main()
     cin >> gpa;
     cout << endl << "Enter credits taken: ";
     cin >> credits;
+    cout << endl << "Enter TA Pay: $";
+    cin >> tapay;
     
-    mystudent.fName(fName);
-    mystudent.lName(lName);
-    mystudent.GPA(gpa);
-    mystudent.creditsTaken(credits);
-    mystudent.standing(credits);
-    displayData(mystudent);
+    mygradstudent.fName(fName);
+    mygradstudent.lName(lName);
+    mygradstudent.GPA(gpa);
+    mygradstudent.creditsTaken(credits);
+    mygradstudent.standing(credits);
+    mygradstudent.TApay(tapay);
+    displayData(mygradstudent);
     
     return 0;
 }
